e1eeee694c4d14ad7190ebbf6c7361e7d954d88d
hanif354 <75107950+hanif354-bayte@users.noreply.github.com>
2023-10-30 22:39:00 +0700

    Update views.py1

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ def home(request):
     # Query semua produk dari database
     products = request.dbsession.query(Product).all()
     # Kembalikan produk dalam format JSON
-    return {'products': products}
+    return {'products': [product.serialize() for product in products]}
 
 # Definisikan view untuk menambahkan produk
 @view_config(route_name='add_product', request_method='POST', renderer='json')
@@ -39,4 +39,39 @@ def delete_product(request):
     else:
         return {'status': 'error', 'message': 'Product not found'}
 
-# Buat view dan route untuk update dan pembelian produk
+# Definisikan view untuk memperbarui produk
+@view_config(route_name='update_product', request_method='PUT', renderer='json')
+def update_product(request):
+    # Ambil id produk dari path parameter
+    product_id = int(request.matchdict['id'])
+    # Query produk berdasarkan id
+    product = request.dbsession.query(Product).get(product_id)
+    # Perbarui atribut produk jika ditemukan
+    if product:
+        # Ambil data JSON dari permintaan
+        data = request.json_body
+        # Lakukan validasi data di sini jika diperlukan
+        # Perbarui atribut produk
+        product.name = data.get('name', product.name)
+        product.price = data.get('price', product.price)
+        product.stock = data.get('stock', product.stock)
+        # Kembalikan respons JSON
+        return {'status': 'success', 'message': 'Product updated successfully'}
+    else:
+        return {'status': 'error', 'message': 'Product not found'}
+
+# Definisikan view untuk pembelian produk
+@view_config(route_name='purchase_product', request_method='POST', renderer='json')
+def purchase_product(request):
+    # Ambil id produk dari path parameter
+    product_id = int(request.matchdict['id'])
+    # Query produk berdasarkan id
+    product = request.dbsession.query(Product).get(product_id)
+    # Lakukan validasi stok produk dan proses pembelian jika ditemukan
+    if product and product.stock > 0:
+        # Kurangi stok produk
+        product.stock -= 1
+        # Kembalikan respons JSON
+        return {'status': 'success', 'message': 'Product purchased successfully'}
+    else:
+        return {'status': 'error', 'message': 'Product not available for purchase'}
